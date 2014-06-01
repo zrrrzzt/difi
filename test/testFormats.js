23e6b3fbaaa4a9322fcde845a5761a4f13db7d4d
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-06-01 15:15:59 +0200

    Updates tests

--- test/testFormats.js
+++ test/testFormats.js
@@ -11,42 +11,42 @@ var difi = require('../index')
 
 describe('Difi - formats', function(){
 
-  it('Should have a length of 572 if format is csv', function(done){
+  it('Should have a length of 626 if format is csv', function(done){
     opts.format = 'csv';
     difi(opts, function(err, data){
-      assert.equal(data.length, 572);
+      assert.equal(data.length, 626);
       done();
     });
   });
 
-  it('Should have a length of 616 if format is json', function(done){
+  it('Should have a length of 670 if format is json', function(done){
     opts.format = 'json';
     difi(opts, function(err, data){
-      assert.equal(data.length, 616);
+      assert.equal(data.length, 670);
       done();
     });
   });
 
-  it('Should have a length of 627 if format is jsonp', function(done){
+  it('Should have a length of 681 if format is jsonp', function(done){
     opts.format = 'jsonp';
     difi(opts, function(err, data){
-      assert.equal(data.length, 627);
+      assert.equal(data.length, 681);
       done();
     });
   });
 
-  it('Should have a length of 1380 if format is xml', function(done){
+  it('Should have a length of 1494 if format is xml', function(done){
     opts.format = 'xml';
     difi(opts, function(err, data){
-      assert.equal(data.length, 1380);
+      assert.equal(data.length, 1494);
       done();
     });
   });
 
-  it('Should have a length of 589 if format is yaml', function(done){
+  it('Should have a length of 646 if format is yaml', function(done){
     opts.format = 'yaml';
     difi(opts, function(err, data){
-      assert.equal(data.length, 589);
+      assert.equal(data.length, 646);
       done();
     });
   });
