2a949557894da14af1181c51310a09e84ea0e8aa
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-02-24 21:21:16 +0100

    Fixes for jscs

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ var validFormats = ['csv', 'json', 'jsonp', 'xml', 'yaml'];
 var apiUrl = 'http://hotell.difi.no/api';
 var data = '';
 
-module.exports = function(opts, callback){
+module.exports = function(opts, callback) {
 
-  if(!opts.dataset){
+  if (!opts.dataset) {
     return callback(new Error('Missing required param: dataset'), null);
   }
 
-  if(!opts.format){
+  if (!opts.format) {
     return callback(new Error('Missing required param: format'), null);
   }
 
-  if(validFormats.indexOf(opts.format) < 0){
+  if (validFormats.indexOf(opts.format) < 0) {
     return callback(new Error('Illegal format requested'), null);
   }
 
-  if(!opts.query){
+  if (!opts.query) {
     return callback(new Error('Missing required param: query'), null);
   }
 
   var uri = util.format('%s/%s/%s', apiUrl, opts.format, opts.dataset);
 
-  request(uri, {qs:opts.query}, function(error, response, body){
-    if(error) {
+  request(uri, {qs:opts.query}, function(error, response, body) {
+    if (error) {
       return callback(error, null);
     }
     data = opts.format === 'json' ? JSON.parse(body.toString()) : body.toString();
