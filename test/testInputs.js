82d1e765520812cb0a6724ddb17648e92781ae3a
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-02-24 21:04:31 +0100

    Formats code to please jshint

--- test/testInputs.js
+++ test/testInputs.js
@@ -1,8 +1,7 @@
 'use strict';
 
-var difi = require('../index')
-  , assert = require('assert')
-  ;
+var difi = require('../index');
+var assert = require('assert');
 
 describe('Difi - inputs', function(){
 
@@ -10,13 +9,17 @@ describe('Difi - inputs', function(){
     var opts = {'format':'json'};
     difi(opts, function(err, data){
       assert.throws(function(){
-          if(err) throw err;
+          if(err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
       }, function(err){
         if((err instanceof Error) && /Missing required param: dataset/.test(err)){
-          return true
+          return true;
         }
       },
-      "Unexpected error"
+      'Unexpected error'
       );
       done();
     });
@@ -26,13 +29,17 @@ describe('Difi - inputs', function(){
     var opts = {'dataset':'brreg/enhetsregisteret'};
     difi(opts, function(err, data){
       assert.throws(function(){
-          if(err) throw err;
+          if(err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
         }, function(err){
           if((err instanceof Error) && /Missing required param: format/.test(err)){
-            return true
+            return true;
           }
         },
-        "Unexpected error"
+        'Unexpected error'
       );
       done();
     });
@@ -42,13 +49,17 @@ describe('Difi - inputs', function(){
     var opts = {'dataset':'brreg/enhetsregisteret', format:'cucumber'};
     difi(opts, function(err, data){
       assert.throws(function(){
-          if(err) throw err;
+          if(err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
         }, function(err){
           if((err instanceof Error) && /Illegal format requested/.test(err)){
-            return true
+            return true;
           }
         },
-        "Unexpected error"
+        'Unexpected error'
       );
       done();
     });
@@ -58,13 +69,17 @@ describe('Difi - inputs', function(){
     var opts = {'dataset':'brreg/enhetsregisteret', format:'json'};
     difi(opts, function(err, data){
       assert.throws(function(){
-          if(err) throw err;
+          if(err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
         }, function(err){
           if((err instanceof Error) && /Missing required param: query/.test(err)){
-            return true
+            return true;
           }
         },
-        "Unexpected error"
+        'Unexpected error'
       );
       done();
     });
@@ -73,7 +88,9 @@ describe('Difi - inputs', function(){
   it('Should return a message if dataset is not found', function(done){
     var opts = {dataset:'npmlovesyou', format:'json', query:{query:'doyoulovenpm'}};
     difi(opts, function(err, data){
-      if(err) throw err;
+      if(err) {
+        throw err;
+      }
       assert.equal(data.message, 'Dataset or folder not found.');
       done();
     });
