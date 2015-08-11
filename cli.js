e7394c3913a9d903a683366eb69b2aa6111acace
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-08-12 00:40:04 +0200

    Changes to JavaScript Standard Style

--- cli.js
+++ cli.js
@@ -1,48 +1,46 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-var difi = require('./index');
-var pkg = require('./package.json');
-var query = process.argv[2];
-var argv = require('minimist')((process.argv.slice(2)));
+var difi = require('./index')
+var pkg = require('./package.json')
+var query = process.argv[2]
+var argv = require('minimist')((process.argv.slice(2)))
 var opts = {
-      dataset: argv.dataset,
-      format: argv.format || 'yaml',
-      query: {
-        query:argv.query
-      }
-};
+  dataset: argv.dataset,
+  format: argv.format || 'yaml',
+  query: {
+    query: argv.query
+  }
+}
 
-function printHelp() {
-  console.log(pkg.description);
-  console.log('');
-  console.log('Usage:');
-  console.log('');
-  console.log(' $ difi <dataset> --query=<query>');
-  console.log('');
-  console.log('Optionally pass return format');
-  console.log('Available formats: csv, json, jsonp, xml or yaml (default)');
-  console.log('');
-  console.log(' $ difi <dataset> --query=<query> --format=<format>');
+function printHelp () {
+  console.log(pkg.description)
+  console.log('')
+  console.log('Usage:')
+  console.log('')
+  console.log(' $ difi <dataset> --query=<query>')
+  console.log('')
+  console.log('Optionally pass return format')
+  console.log('Available formats: csv, json, jsonp, xml or yaml (default)')
+  console.log('')
+  console.log(' $ difi <dataset> --query=<query> --format=<format>')
 }
 
 if (!query || process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
-  printHelp();
-  return;
+  printHelp()
 }
 
 if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -1) {
-  console.log(pkg.version);
-  return;
+  console.log(pkg.version)
 }
 
 if (query.indexOf('--') === -1) {
-  opts.dataset = argv._[0];
+  opts.dataset = argv._[0]
 }
 
-difi(opts, function(err, data) {
-  if (err) {
-    throw err;
+difi(opts, function (error, data) {
+  if (error) {
+    throw error
   }
-  console.log(data);
-});
+  console.log(data)
+})
--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-'use strict';
+'use strict'
 
-var request = require('request');
-var util = require('util');
-var validFormats = ['csv', 'json', 'jsonp', 'xml', 'yaml'];
-var apiUrl = 'http://hotell.difi.no/api';
-var data = '';
+var request = require('request')
+var util = require('util')
+var validFormats = ['csv', 'json', 'jsonp', 'xml', 'yaml']
+var apiUrl = 'http://hotell.difi.no/api'
+var data = ''
 
-module.exports = function(opts, callback) {
+module.exports = function (opts, callback) {
 
   if (!opts.dataset) {
-    return callback(new Error('Missing required param: dataset'), null);
+    return callback(new Error('Missing required param: dataset'), null)
   }
 
   if (!opts.format) {
-    return callback(new Error('Missing required param: format'), null);
+    return callback(new Error('Missing required param: format'), null)
   }
 
   if (validFormats.indexOf(opts.format) < 0) {
-    return callback(new Error('Illegal format requested'), null);
+    return callback(new Error('Illegal format requested'), null)
   }
 
   if (!opts.query) {
-    return callback(new Error('Missing required param: query'), null);
+    return callback(new Error('Missing required param: query'), null)
   }
 
-  var uri = util.format('%s/%s/%s', apiUrl, opts.format, opts.dataset);
+  var uri = util.format('%s/%s/%s', apiUrl, opts.format, opts.dataset)
 
-  request(uri, {qs:opts.query}, function(error, response, body) {
+  request(uri, {qs: opts.query}, function (error, response, body) {
     if (error) {
-      return callback(error, null);
+      return callback(error, null)
     }
-    data = opts.format === 'json' ? JSON.parse(body.toString()) : body.toString();
-    return callback(null, data);
-  });
+    data = opts.format === 'json' ? JSON.parse(body.toString()) : body.toString()
+    return callback(null, data)
+  })
 
-};
+}
